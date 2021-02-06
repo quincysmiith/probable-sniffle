a9ca1f115f08054e743cb3c05a800fa592b9fe4c
Fuzzy Lump <8ca53525@opayq.com>
2021-02-06 13:51:10 +1100

    removed references to databases

--- app/__init__.py
+++ app/__init__.py
@@ -1,14 +1,11 @@
 from flask import Flask, Blueprint
 
 from .views import main
-from .extensions import db
 
 def create_app(config_file='settings.py'):
     app = Flask(__name__)
 
     app.config.from_pyfile(config_file)
-
-    db.init_app(app)
 
     app.register_blueprint(main)
 

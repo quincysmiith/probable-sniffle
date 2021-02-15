8db93178fdc3de1469934cfc970fca7250d07cdc
Fuzzy Lump <8ca53525@opayq.com>
2021-02-15 20:26:27 +1100

    added matomo tracking to base html. added logic on /weightlog endpoint to save to Google Spreadsheeet. changed healthlog.html to submit form to healthlog endpoint.

--- app/helpers.py
+++ app/helpers.py
@@ -4,7 +4,7 @@ import json
 import gspread
 from google.oauth2 import service_account
 
-def save_google_creds_to_disk():
+def google_sheets_connection():
     """Helper function to pull create json credentials
     file in temporary file and authorise gspread library
     for Google Sheets manipulation.
@@ -14,18 +14,16 @@ def save_google_creds_to_disk():
         gc = gspread.service_account()
     """
 
-my_dict = {}
+    my_dict = {}
 
-my_dict['type'] = os.getenv("TYPE")
-my_dict['project_id'] = os.getenv("PROJECT_ID")
-my_dict['private_key_id'] = os.getenv("PRIVATE_KEY_ID")
-my_dict['private_key'] = os.getenv("PRIVATE_KEY")
-my_dict['client_email'] = os.getenv("CLIENT_EMAIL")
-my_dict['client_id'] = os.getenv("CLIENT_ID")
-my_dict['auth_uri'] = os.getenv("AUTH_URI")
-my_dict['token_uri'] = os.getenv("TOKEN_URI")
-my_dict['auth_provider_x509_cert_url'] = os.getenv("AUTH_PROVIDER_X509_CERT_URL")
-my_dict['client_x509_cert_url'] = os.getenv("CLIENT_X509_CERT_URL")
+    my_dict['type'] = os.getenv("TYPE")
+    my_dict['project_id'] = os.getenv("PROJECT_ID")
+    my_dict['private_key_id'] = os.getenv("PRIVATE_KEY_ID")
+    my_dict['private_key'] = os.getenv("PRIVATE_KEY")
+    my_dict['client_email'] = os.getenv("CLIENT_EMAIL")
+    my_dict['client_id'] = os.getenv("CLIEhttps://gspread.readthedocs.io/en/latest/oauth2.html#for-bots-using-service-accountN_URI")
+    my_dict['auth_provider_x509_cert_url'] = os.getenv("AUTH_PROVIDER_X509_CERT_URL")
+    my_dict['client_x509_cert_url'] = os.getenv("CLIENT_X509_CERT_URL")
 
 
     my_temp = tempfile.NamedTemporaryFile('w+')
@@ -39,12 +37,17 @@ my_dict['client_x509_cert_url'] = os.getenv("CLIENT_X509_CERT_URL")
     'https://www.googleapis.com/auth/drive'
     ]
 
+    
+
     credentials = service_account.Credentials.from_service_account_info(
         json.loads(
-            os.environ['GOOGLE_API_CREDS']),
+            os.environ['GOOGLE_API_CREDS2']),
         scopes = scopes)
 
     gc = gspread.authorize(credentials)
+    
+
+    #gc = gspread.service_account(filename='/home/fuzzy/Documents/Projects/flask_app_platform/marquin-personal-tools-5f84ef73756b.json')
 
     return gc
 
--- app/views.py
+++ app/views.py
@@ -1,6 +1,7 @@
 from flask import Blueprint, render_template, redirect, url_for
 from flask.helpers import url_for
 from .forms import PercDiffForm, WeightForm
+from .helpers import google_sheets_connection
 
 main = Blueprint('main', __name__)
 
@@ -16,18 +17,26 @@ def render():
 
 
 
-@main.route('/healthlog')
+@main.route('/weightlog', methods = ["GET", "POST"])
 def log_health_page():
 
     weight_form = WeightForm()
 
+    if weight_form.validate_on_submit():
+        gc = google_sheets_connection()
+        sh = gc.open("Weight Logs")
+        worksheet = sh.worksheet("Sheet1")
+
+        user = weight_form.user.data
+        weight = weight_form.weight.data
+        my_date = weight_form.date.data
+
+        worksheet.append_row([user, my_date, weight])
+
+        return redirect(url_for('main.log_health_page'))
+
+    
+
     # exercise_form = 
 
     return render_template("healthlog.html", weight_form = weight_form)
-
-
-@main.route('/weightlog', methods = ['POST'])
-def save_weight():
-    # some logic to save entered data
-
-    return redirect(url_for('main.log_health_page'))
